1bc077cd3cef93a1aea477e5c199ebb36d8073bc
chk7082 <whgusrl7082@gmail.com>
2023-03-14 15:40:59 +0900

    0314 Django Start

---
+++ 0314 Django Start/홈워크_django_01_hw01_P.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from my_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('my_app/', views.index),
+]
